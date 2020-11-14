74a8488205c154e26a1b3e671c6ec7770fde9c24
Wojtek Porczyk <woju@invisiblethingslab.com>
2020-11-14 21:54:09 +0100

    Revert setup.cfg for compatibility with xenial
    Python and/or pip and/or setuptools on Ubuntu 16.04 is too old and does
    not understand setup.cfg.

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 import setuptools
+from prometheus_sgx_exporter import __version__ as _version
+
 setuptools.setup(
+    name='prometheus_sgx_exporter',
+    version=_version,
+    packages=setuptools.find_packages(),
     ext_modules=[
         setuptools.Extension('prometheus_sgx_exporter._cpuid',
             sources=['prometheus_sgx_exporter/_cpuidmodule.c'],
             extra_compile_args=['-masm=intel', '-Werror'],
         ),
     ],
+    data_files=[
+        ('lib/systemd/system', ['prometheus-sgx-exporter.service']),
+    ],
+    entry_points={
+        'console_scripts': [
+            'prometheus-sgx-exporter = prometheus_sgx_exporter.__main__:main',
+        ],
+    },
 )
